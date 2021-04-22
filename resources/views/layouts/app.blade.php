a7b19d12d1f47b6f4340efb08d191d6204139947
unbreakablery <unbreakablery@gmail.com>
2021-04-23 08:54:10 +0900

    added author in footer from env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                 <div class="content py-3">
                     <div class="row font-size-sm">
                         <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-right">
-                            Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="" target="_blank">Anthony</a>
+                            Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="" target="_blank">{{ env('AUTHOR') }}</a>
                         </div>
                         <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-left">
                             <a class="font-w600" href="{{ route('dashboard') }}" target="_blank">{{ config('app.name') }}</a> &copy; <span data-toggle="year-copy"></span>
