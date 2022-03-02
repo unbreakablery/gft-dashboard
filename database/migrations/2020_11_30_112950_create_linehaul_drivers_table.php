c82c5419cd5202b42e9e147cd9e977a2cfbba0f0
unbreakablery <unbreakablery@gmail.com>
2022-03-03 00:10:59 +0900

    added photo field into drivers table

--- database/migrations/2020_11_30_112950_create_linehaul_drivers_table.php
+++ database/migrations/2020_11_30_112950_create_linehaul_drivers_table.php
@@ -21,6 +21,7 @@ class CreateLinehaulDriversTable extends Migration
             $table->string('phone')->nullable();
             $table->string('license')->nullable();
             $table->string('address')->nullable();
+            $table->string('photo')->nullable();
             $table->float('price_per_mile', 8, 4)->default(0.0000);
             $table->tinyInteger('work_status')->default('1');
             $table->integer('company_id');
